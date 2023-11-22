ead1435f02970594b81374063dc6bc3e242feb68
Pauline Ona <paulinemaeona@gmail.com>
2023-11-22 17:23:03 +0800

    Added more features

--- asynchronous.write.js
+++ asynchronous.write.js
@@ -3,8 +3,9 @@ const { constants, writeFile } = require('fs');
 
 // Write a string to the file 'app.log' asynchronously
 writeFile('./data/newapp.log', '163.3.217.18 - - [21/09/2019:10:07:21 -0500] "GET /write-file-test2" 405 21512', {
-    flag: 'wx'//exclusive write - will fail if file already exists
+    // flag: 'wx'//exclusive write - will fail if file already exists
     // mode: constants.S_IWUSR | constants.S_IRUSR // exclusive write and read permissions for the user
+    // encoding: 'ascii' // Specifies that the file data should be encoded or decoded
 }, (err) => {
     // If there is an error writing to the file, log the error message
     if (err) {
